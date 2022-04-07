57d646e94e2c47244e10f49eedcf2dda85d74fa3
kshiris-BRONGA <88771859+kshiris-BRONGA@users.noreply.github.com>
2022-04-07 14:20:07 +0530

    Create job_dsl_send_email.groovy

---
+++ job_dsl_send_email.groovy
@@ -0,0 +1,45 @@
+job('send-email-to-suspended-users') {
+
+authorization {
+    permission('hudson.model.Item.Build:authenticated')
+    permission('hudson.model.Item.Cancel:authenticated')
+}
+
+
+
+steps {
+    shell('''\
+        #!/bin/bash
+        cd /home/jenkins/suspended_users
+        MAIL_IDS=$(tail -n +2 suspended_users.csv | cut -d, -f 1 | sed 's/^S-.*//' | tr -s '\n' ' ' | sed 's/ /@gap.com, /g' | sed 's/,.$//')
+        echo MAIL_IDS=$MAIL_IDS > ${WORKSPACE}/mails.properties
+        echo $MAIL_IDS
+        cat ${WORKSPACE}/mails.properties
+        '''.stripIndent())
+}
+
+environmentVariables {
+    propertiesFile('mails.properties')
+    keepBuildVariables(true)
+}
+
+publishers {
+        extendedEmail {
+            recipientList('$MAIL_IDS')
+            defaultSubject('ALERT!! GitHub account suspended ')
+            defaultContent('Ways to Reactivate/Unsuspend the account')
+            contentType('text/html')
+            triggers {
+                success {
+                    
+                    subject('ALERT!! Your GitHub account suspended')
+                    content('Hi GitHub User,<br><br>Your GitHub account is suspended because you are inactive since 90 days on Github. If you want to reactivate please refer the confluence page <br> https://confluence.gapinc.com/display/CICD/How+to+re-activate+suspended+account <br><br>Regards,<br>CI-CD Platform Team.' )
+                    sendTo { recipientList() }
+                }
+            }
+        }
+    }
+    
+label('pipes-docker-agent')
+
+}
